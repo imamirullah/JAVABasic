e89a8bb0a92c7d541659b3c2a4aee6d43fb272ee
Amir <ujala@apples-MacBook-Air.local>
2024-09-28 15:29:03 +0530

    updated

--- loop.java
+++ loop.java
@@ -2,28 +2,30 @@ import java.util.Scanner;
 
 public class loop {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int range = sc.nextInt();
-        int counter = 1;
+        // Scanner sc = new Scanner(System.in);
+        // int range = sc.nextInt();
+        // int counter = 1;
 
         // While loop
 
         // while (counter <= range) {
-        //     System.out.print(counter + " ");
-        //     counter++;
+        // System.out.print(counter + " ");
+        // counter++;
         // }
 
         // for loop
         // for(counter=1; counter<range; counter++){
-        //     System.out.print(counter + " ");
+        // System.out.print(counter + " ");
         // }
 
-        for(int line=1; line<=4; line++){
-            for (int j=1; j<4; j++){
-
-                System.err.println("****");
+        // Outer loop for the number of lines
+        for (int line = 1; line <= 4; line++) {
+            // Inner loop for printing stars
+            for (int j = 1; j <= line; j++) {
+                System.out.print(" *");
             }
+            // Move to the next line after printing stars for the current line
+            System.out.println();
         }
-
     }
 }
